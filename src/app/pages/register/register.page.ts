eae2e747a96bb5bfc81a8adaaca5bdb53c4897d6
SpolancoC <sm.polancoc@gmail.com>
2024-11-22 14:08:04 -0300

    Pages desing

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,13 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage {
-  nombre: string = '';
-  apellido: string = '';
+  firstName: string = '';
+  lastName: string = '';
   email: string = '';
   password: string = '';
   confirmPassword: string = '';
 
-  constructor() {}
+  constructor(
+    private router: Router
+  ) {}
 
-  
+  redirect_login(){
+    this.router.navigate(['/login']);
+  }
+
 }
